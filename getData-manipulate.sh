e2f0b56f69ccc369dc78bb19ee2ec0a8ab067c2d
tmeir <66691248+tmeir@users.noreply.github.com>
2022-09-24 12:22:03 +0300

    Update getData-manipulate.sh

--- getData-manipulate.sh
+++ getData-manipulate.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-cat toManipulate.txt |urldedupe |grep -e "\.php\|\.asp\|\.htm\|\.txt\|\.xml\|\.asm\|\.jsp\|\.shtm" >> ~/Bounty/params/files-found/toManipulate.txt-files.txt
-cat toManipulate.txt |urldedupe |grep -e "\.js" >> ~/Bounty/params/files-found/toManipulate.txt-jsfiles.txt
+cat toManipulate.txt |urldedupe |grep -e "\.php\|\.asp\|\.htm\|\.txt\|\.xml\|\.asm\|\.jsp\|\.shtm" >> ~/Bounty/params/files-found/files-toManipulate.txt
+cat toManipulate.txt |urldedupe |grep -Eav ".js|.css|.jpg|.avi|.png|.jpeg|.webp|.woff|.bmp|.php|.asp|.htm|.txt|.xml|.asm|.jsp|.shtm" >> ~/Bounty/params/files-found/AllOtherfiles-toManipulate.txt
+cat toManipulate.txt |urldedupe |grep -e "\.js" >> ~/Bounty/params/files-found/jsfiles-toManipulate.txt
 cat toManipulate.txt |urldedupe |grep "=" >> ~/Bounty/params/all-toManipulate.txt-params.txt
-cat toManipulate.txt |urldedupe |grep -Eav ".js|.css|.jpg|.avi|.png|.jpeg|.webp|.woff|.bmp" |grep "=" >~/Bounty/params/toManipulate.txt-params.txt
+cat toManipulate.txt |urldedupe |grep -Eav ".js|.css|.jpg|.avi|.png|.jpeg|.webp|.woff|.bmp" |grep "=" >~/Bounty/params/toManipulate-params.txt
 
 #cat $@ |urldedupe |grep -e "\.php\|\.asp\|\.htm\|\.txt\|\.xml\|\.asm\|\.jsp\|\.shtm" >> /mnt/vmshares/params/files-found/$@-files.txt
 #cat $@ |urldedupe |grep -e "\.js" >> /mnt/vmshares/params/files-found/$@-jsfiles.txt
